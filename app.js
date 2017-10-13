2a04b2addce22e6a9c00cbf97dd12f15d0ed9a6a
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-10-13 19:18:47 -0400

    fixed bodyparser issue - renamed all instances to 'bodyparser'

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+const users = require('./routes/users');
+
 // Initialize app
 const app = express();
 
@@ -16,12 +18,18 @@ const port = 3000;
 // Allow Cross Origin Resource Sharing
 app.use(cors());
 
-// Return response when root is requested
+// Enable Body Parser Middleware
+app.use(bodyparser.json());
+
+// Index route
 app.get('/', (req, res)=> {
 	res.send('Invalid endpoint!');
 });
 
-// Listed on defined port
+// Users.js handles users routes
+app.use('/users', users);
+
+// Start server on defined port
 app.listen(port, () => {
 	console.log('Server started on port ' + port);
 });
