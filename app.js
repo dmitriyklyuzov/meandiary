ed2f33bed9163962389bd70dfea0f601a0873fed
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-10-13 19:08:28 -0400

    added comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Just like web.php
+// Just like routes/web.php
 
 const express = require('express');
 const path = require('path');
@@ -7,14 +7,21 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+// Initialize app
 const app = express();
 
+// Define port
 const port = 3000;
 
+// Allow Cross Origin Resource Sharing
+app.use(cors());
+
+// Return response when root is requested
 app.get('/', (req, res)=> {
 	res.send('Invalid endpoint!');
 });
 
+// Listed on defined port
 app.listen(port, () => {
 	console.log('Server started on port ' + port);
 });
