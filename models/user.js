5c2867f686a71c0687801d34f8d97faa89ed5099
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-10-13 21:09:53 -0400

    added 'addUser' function to the user model

--- models/user.js
+++ models/user.js
@@ -12,7 +12,8 @@ const config = require('../config/database');
 // User Schema
 const UserSchema = mongoose.Schema({
 	name: {
-		type: String
+		type: String,
+		required: true
 	},
 	email: {
 		type: String,
@@ -26,3 +27,28 @@ const UserSchema = mongoose.Schema({
 
 // Create User
 const User = module.exports = mongoose.model('User', UserSchema);
+
+// Add user
+module.exports.addUser = function(newUser, callback){
+	bcrypt.genSalt(10, (err, salt)=>{
+		bcrypt.hash(newUser.password, salt, (err, hash)=>{
+			// Throw arror if any
+			if(err) throw err;
+			// Set the new user's pw to the hash
+			newUser.password = hash;
+			// Save the new user
+			newUser.save(callback);
+		});
+	});
+}
+
+// Get user by ID function, export it so it is usable from outside
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+// Get user by email
+module.exports.getUserByEmail = function(id, callback){
+	const query = {email : email}
+	User.findOne(query, callback);f
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+
+// Bring in user model
+const User = require('../models/user');
 
 // Rregister route
 router.post('/register', (req, res, next)=>{
-	res.send('REGISTER');
+	let newUser = new User({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password
+	});
+
+	User.addUser(newUser, (err, user)=>{
+		if(err){
+			res.json({
+				success: false,
+				message: 'Failed to register user'
+			});
+		}
+		else{
+			res.json({
+				success: true,
+				message: 'User successfully registered'
+			});
+		}
+	});
 });
 
 // Authentication route
