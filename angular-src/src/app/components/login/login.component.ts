e5a6523cba9b8e3c8b27dca8bb413109e542f19c
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-10-23 16:32:59 -0400

    added logging in and logging out

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // add email and password as component properties
+  email: String;
+  password: String;
+
+  // inject services and components
+  constructor(
+  	private authService: AuthService,
+  	private router: Router,
+  	private flashMessages: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit(){
+  	console.log('Login form submitted...');
+  	
+  	// create user object
+  	const user = {
+  		email: this.email,
+  		password: this.password
+  	}
+
+  	// call auth service - subscribe to an observable
+  	this.authService.authenticateUser(user).subscribe(data => {
+  		// if successfully logged in
+  		if(data.success){
+  			this.authService.storeUserData(data.token, data.user);
+  			this.flashMessages.show('You are now logged in', {
+  				cssClass: "alert-success",
+  				timeout: 3000
+  			});
+  			this.router.navigate(['dashboard']);
+  		}
+  		else{
+  			// Display a flash message from server
+  			this.flashMessages.show(data.msg, {
+  				cssClass: "alert-danger",
+  				timeout: 3000
+  			});
+  			// redirect to login
+  			this.router.navigate(['login']);
+  		}
+  	});
   }
 
 }
