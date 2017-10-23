0e155a40d186b0d69cbeec16d837570f9fe108ae
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-10-23 17:11:29 -0400

    added loggedIn() function, added to navbar to display different routes to logged in users and guests

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { tokenNotExpired } from 'angular2-jwt';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -35,6 +36,28 @@ export class AuthService {
       .map(res => res.json()); /* returns a token and user info if success:true, else returns success:false */
   }
 
+  // get profile path
+  getUserProfile(){
+    // set a header value
+    let headers = new Headers();
+    // load token from local storage
+    this.loadToken();
+    // set content type header
+    headers.append('Content-Type', 'application/json');
+    // set authorization header
+    headers.append('Authorization', this.authToken);
+
+    // create an observable - make a GET request to the /profile endpoint with headers
+    return this.http.get('http://localhost:3000/users/profile', {headers})
+      .map(res => res.json()); /* returns a token and user info if success:true, else returns success:false */
+  }
+
+  // fetch token from local storage
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
   // store user data
   storeUserData(token, user){
     // store token in local storage (angular-jwt looks for the id_token path by default)
@@ -52,5 +75,10 @@ export class AuthService {
     this.user = null;
     localStorage.clear();
 
+  }
+
+  // check to see if we are logged in (actually to see if the token is not expired)
+  loggedIn(){
+    return tokenNotExpired('id_token');
   }
 }
