0a252d4bb4073d73b84f24f4f494b749fea9ece5
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-10-23 16:33:57 -0400

    added logout link

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  	private authService: AuthService,
+  	private router: Router,
+  	private flashMessages: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLogoutClick(){
+  	// call the logout() function of the AuthService
+  	this.authService.logout();
+  	// Display a flash message
+  	this.flashMessages.show('You are now logged out', {
+  		cssClass: 'alert-success',
+  		timeout: 3000
+  	});
+  	// Redirect to the login page
+  	this.router.navigate(['login']);
+  	return false; 
   }
 
 }
