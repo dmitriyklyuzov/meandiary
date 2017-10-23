1e06557b582a10575e4043783910224c2c27d50f
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-10-23 14:10:39 -0400

    added modules to app

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 // Importing main app component
 import { AppComponent } from './app.component';
@@ -15,6 +16,19 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+// Routes like routes/web.php
+// Every path maps to a Component
+const appRoutes: Routes = [
+  // Home page
+  {path:'', component: HomeComponent},
+  // Register page
+  {path:'register', component: RegisterComponent},
+  // Login page
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'profile', component: ProfileComponent},
+]
 
 @NgModule({
   // Adding main app component into declarations
@@ -30,7 +44,8 @@ import { ProfileComponent } from './components/profile/profile.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
